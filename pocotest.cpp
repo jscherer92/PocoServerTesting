64a29995f362b3de6794cbdcbc71cb7362a5acb3
Justin Scherer <jscherer92@gmail.com>
2019-03-03 14:58:12 -0600

    Added in proper handling for most common file types

--- pocotest.cpp
+++ pocotest.cpp
@@ -14,41 +14,68 @@
 #include "Poco/Dynamic/Var.h"
 #include "Poco/URI.h"
 #include "Poco/Path.h"
+#include "Poco/ExpireLRUCache.h"
+#include "Poco/Timestamp.h"
 
 #include <exception>
 #include <sstream>
 #include <iostream>
 
+struct CacheSetup {
+    long size;
+    Poco::Timestamp::TimeDiff time;
+};
+
 class RootHandler: public Poco::Net::HTTPRequestHandler
 {
 public:
-    RootHandler(std::string staticHostLocation) {
+    RootHandler(std::string staticHostLocation) : defaultRoot("index.html") 
+    {
         staticHost = staticHostLocation;
+    }
+    RootHandler(std::string staticHostLocation, std::string root)
+    {
+        staticHost = staticHostLocation;
+        defaultRoot = root;
     }
     void handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
     {
         Poco::URI uri(request.getURI());
         std::cout << uri.getPath() << std::endl;
-        Poco::Path path(staticHost,  (std::string("/").compare(uri.getPath()) == 0) ? std::string("/index.html") : uri.getPath());
+        std::cout << request.getMethod() << std::endl;
+        Poco::Path path(staticHost,  (std::string("/").compare(uri.getPath()) == 0) ? std::string("/" + defaultRoot) : uri.getPath());
+        std::cout << path.getExtension() << std::endl;
         Poco::File content(path);
         
         if( content.exists() && content.isFile() && content.canRead() ) {
             response.setChunkedTransferEncoding(true);
-            // this needs to be handled better
-            response.setContentType("text/html");
+            response.setContentType(getContentType(path.getExtension()));
             std::ostream& ostr = response.send();
             Poco::FileInputStream fis(path.toString());
             ostr << fis.rdbuf();
         } else {
             response.setChunkedTransferEncoding(true);
-            response.setContentType("text/html");
+            response.setContentType(getContentType(path.getExtension()));
             response.setStatus("404");
             std::ostream& ostr = response.send();
             ostr << "404: File Not Found!";
         }
     }
 private:
+    //Todo: this should be a map lookup so it is an O(1) time
+    std::string getContentType(std::string fileExtension) {
+        if(fileExtension.compare("html") == 0) return std::string("text/html");
+        if(fileExtension.compare("css") == 0) return std::string("text/css");
+        if(fileExtension.compare("js") == 0) return std::string("text/javacsript");
+        if(fileExtension.compare("gif") == 0) return std::string("image/gif");
+        if(fileExtension.compare("ico") == 0) return std::string("image/vnd.microsoft.icon");
+        if(fileExtension.compare("jpeg") == 0 || fileExtension.compare("jpg") == 0) return std::string("image/jpeg");
+        if(fileExtension.compare("json") == 0) return std::string("application/json");
+        if(fileExtension.compare("svg") == 0) return std::string("image/svg+xml");
+        return std::string("text/plain");
+    };
     std::string staticHost;
+    std::string defaultRoot;
 };
 
 class WebSocketHandler: public Poco::Net::HTTPRequestHandler
@@ -72,7 +99,7 @@ public:
 class MyRequestHandlerFactory: public Poco::Net::HTTPRequestHandlerFactory
 {
 public:
-    MyRequestHandlerFactory() : staticLocation("static"), baseDir("static")
+    MyRequestHandlerFactory() : staticLocation("static"), baseDir("static"), defaultRoot("index.html")
     {
     }
 
@@ -82,18 +109,26 @@ public:
         Poco::File staticDirectory(staticLoc);
     }
 
+    MyRequestHandlerFactory(std::string staticLoc, std::string root)
+    {
+        staticLocation = staticLoc;
+        defaultRoot = root;
+        Poco::File staticDirectory(staticLoc);
+    }
+
     Poco::Net::HTTPRequestHandler* createRequestHandler(const Poco::Net::HTTPServerRequest& request)
     {
         if(request.find("Upgrade") != request.end() && Poco::icompare(request["Upgrade"], "websocket") == 0)
             return new WebSocketHandler();
         else {
-            return new RootHandler(staticLocation);
+            return new RootHandler(staticLocation, defaultRoot);
         }
             
     }
 private:
     Poco::File baseDir;
     std::string staticLocation;
+    std::string defaultRoot;
 };
 
 Poco::JSON::Object::Ptr readServerConfiguration(std::string filename) {
@@ -118,18 +153,28 @@ int main(int argc, char** argv)
     Poco::UInt16 port;
     int maxQueued;
     int maxThreads;
+    Poco::Timestamp::TimeDiff maxCacheTime;
+    long maxCacheLength;
     object->get("port").convert(port);
     object->get("max_queued").convert(maxQueued);
     object->get("max_threads").convert(maxThreads);
+    object->get("cache_time_length").convert(maxCacheTime);
+    object->get("cache_max_entries").convert(maxCacheLength);
     std::string staticLocation = object->get("static_files").toString();
+    std::string defaultRoot = object->get("default_root").toString();
 
     Poco::Net::HTTPServerParams* params = new Poco::Net::HTTPServerParams();
     params->setMaxQueued(maxQueued);
     params->setMaxThreads(maxThreads);
 
+    CacheSetup cSetup;
+    cSetup.size = maxCacheLength;
+    cSetup.time = maxCacheTime;
+    
+
     Poco::Net::ServerSocket svs(port);
 
-    Poco::Net::HTTPServer srv(new MyRequestHandlerFactory(staticLocation), svs, params);
+    Poco::Net::HTTPServer srv(new MyRequestHandlerFactory(staticLocation, defaultRoot), svs, params);
 
     srv.start();
 
